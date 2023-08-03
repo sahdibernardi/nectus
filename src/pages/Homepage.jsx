a3219cb62891159f1b3b7fc944f464834c2f34f0
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-08-03 07:38:44 -0300

    section 5

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,9 @@ import headline from '../assets/Headline.svg'
 import nectusLogo from '../assets/NectusLogo.png'
 import youngMen from '../assets/young-men.png'
 import graphicNectusLogo from '../assets/graphic-logo.png'
+import smilingGuy from '../assets/smilingGuy.png'
+import nectusLogoGreen from '../assets/nectusLogoGreen.png'
+import smilingGirl from '../assets/smilingGirl.png'
 
 function Homepage() {
 
@@ -44,6 +47,68 @@ function Homepage() {
         <p className='s2-text'>
           Aqui, fortalecemos a saúde das empresas de forma leve e fluida. Inserimos cada negócio em uma cultura que gera ambientes produtivos, engajados e que promove a sensação de pertencimento, essencial para empresas que querem deixar a sua marca na sociedade e na vida das pessoas.
         </p>
+      </div>
+    </div>
+    <div className='s5'>
+      <div className='s5-div'>
+        <span className='s5-title'>
+          QUEM SOMOS?
+        </span>
+        <div className='s5-subtitle'>
+          <div className='s5-image-bg'>
+            <img alt='smiling guy' src={ smilingGuy } className='s5-img'/>
+          </div>
+          <div className='s5-sb-text'>
+            <span id='s4-text'>
+            Estamos aqui para <span id="s2-hl">cuidar de você por completo.</span>
+            </span>
+          </div>
+        </div>
+        <div className='s5-text-div'>
+          <p className='s2-text'>
+            Cuidamos da saúde de pessoas e empresas com gestão inteligente.  Visionamos bem-estar e engajamento que alavancam a produtividade, gerando ambientes  de felicidade com maior senso de pertencimento. Visão integral em saúde organizacional focada em resultados e sucesso a longo prazo.
+          </p>
+        </div>
+      </div>
+      <div className='s5-div'>
+        <span className='s5-title'>
+          QUAL O NOSSO OBJETIVO?
+        </span>
+        <div className='s5-subtitle'>
+          <div className='s5-logo-bg'>
+            <img alt='nectus logo green' src={ nectusLogoGreen } className='s5-logo'/>
+          </div>
+          <div className='s5-sb-text'>
+            <span id='s4-text'>
+            Aqui <span id="s2-hl">todas as esferas e pilares importam.</span>
+            </span>
+          </div>
+        </div>
+        <div className='s5-text-div'>
+          <p className='s2-text'>
+            Cuidamos da saúde de pessoas e empresas com gestão inteligente.  Visionamos bem-estar e engajamento que alavancam a produtividade, gerando ambientes  de felicidade com maior senso de pertencimento. Visão integral em saúde organizacional focada em resultados e sucesso a longo prazo.
+          </p>
+        </div>
+      </div>
+      <div className='s5-div'>
+        <span className='s5-title'>
+          QUEM SOMOS?
+        </span>
+        <div className='s5-subtitle'>
+          <div className='s5-image-bg'>
+            <img alt='smiling girl' src={ smilingGirl } className='s5-img'/>
+          </div>
+          <div className='s5-sb-text'>
+            <span id='s4-text'>
+            Estamos aqui para <span id="s2-hl">cuidar de você por completo.</span>
+            </span>
+          </div>
+        </div>
+        <div className='s5-text-div'>
+          <p className='s2-text'>
+            Cuidamos da saúde de pessoas e empresas com gestão inteligente.  Visionamos bem-estar e engajamento que alavancam a produtividade, gerando ambientes  de felicidade com maior senso de pertencimento. Visão integral em saúde organizacional focada em resultados e sucesso a longo prazo.
+          </p>
+        </div>
       </div>
     </div>
     </>
