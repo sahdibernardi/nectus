e7598c86d6d7af7caa86bd189c197e6347940fe4
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-10-11 02:22:41 -0300

    add: error 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import './style/header.css'
 import './style/articles.css'
 import './style/forms.css'
 import './style/contact.css'
+import './style/pageNotFound.css'
 
 import Homepage from './pages/Homepage'
 import People from './pages/People'
 import Professionals from './pages/Professionals'
 import Companies from './pages/Companies'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true }}>
        <Routes>
            <Route Component = { Homepage }  path="/" exact />
            <Route Component = { People }  path="/for-people"/>
            <Route Component = { Professionals }  path="/for-professionals" />
            <Route Component = { Companies }  path="/for-companies" />
+           <Route path="*" element={<PageNotFound />} />
         </Routes>
        </BrowserRouter>
     </>
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import graphicNectusLogo from '../assets/graphic-logo.png'
+
+function PageNotFound() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
+
+    return (
+        <>
+        <Header />
+        <div className='s2'>
+            <img alt='graphic nectus logo' src={ graphicNectusLogo } className='graphic-logo' id="graphic-left"/>
+            <div className='s2-title'>
+                <span>
+                Erro 404
+                <br></br>
+                <span id="s2-hl">
+                    Página não encontrada
+                </span>
+                </span>
+                <p className='s2-text'>
+                    Pedimos desculpas pela inconveniência e transtorno gerado, saiba que estamos trabalhando com amor e dedicação para estabilizar e a voltar agregar mais em sua vida e organização.
+                </p>
+                <p className='s2-text'>
+                    A Nectus é a sua parceira, para mais dúvidas, sugestões, sobre nossos serviços, entre em contato com o nosso suporte!
+                </p>
+                <p className='s2-text'>
+                    Ficaremos felizes em lhe atender! Obrigada(o) pela compreensão!✨
+                </p>
+            </div>
+            <img alt='graphic nectus logo' src={ graphicNectusLogo } className='graphic-logo' id="graphic-right"/>
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+export default PageNotFound;
